850db5ef14c377807bb44211745bb8602a6c74bc
Andreas Källberg <anka.213@gmail.com>
2016-02-29 18:17:44 +0100

    Add back keenspot.com
    For some reason, the @include line was missing for keenspot

--- webcomic_reader.user.js
+++ webcomic_reader.user.js
@@ -43,7 +43,7 @@ var defaultSettings = {
 // ==UserScript==
 // @name           Webcomic Reader
 // @author         Javier Lopez <ameboide@gmail.com> https://github.com/ameboide , fork by v4Lo https://github.com/v4Lo and by anka-213 http://github.com/anka-213
-// @version        2016.02.29-1
+// @version        2016.02.29-2
 // @namespace      http://userscripts.org/scripts/show/59842
 // @description    Can work on almost any webcomic/manga page, preloads 5 or more pages ahead (or behind), navigates via ajax for instant-page-change, lets you use the keyboard, remembers your progress, and it's relatively easy to add new sites
 // @homepageURL    https://github.com/anka-213/webcomic_reader#readme
@@ -805,6 +805,7 @@ var defaultSettings = {
 // @include        http://hentaihere.com/m/*/*/*
 // @include        http://gomanga.co/reader/read*
 // @include        http://mangafap.com/image/*
+// @include        http://*.keenspot.com/*
 // ==/UserScript==
 
 var dataCache = null; //cache para no leer del disco y parsear la configuracion en cada getData
