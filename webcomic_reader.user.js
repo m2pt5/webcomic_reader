03757ced1330f458300704aa4ab685f89671b8cb
Andreas Källberg <anka.213@gmail.com>
2016-08-29 22:07:57 +0200

    Fix thehiveworks code. Fixes #25

--- webcomic_reader.user.js
+++ webcomic_reader.user.js
@@ -43,7 +43,7 @@ var defaultSettings = {
 // ==UserScript==
 // @name           Webcomic Reader
 // @author         Javier Lopez <ameboide@gmail.com> https://github.com/ameboide , fork by v4Lo https://github.com/v4Lo and by anka-213 http://github.com/anka-213
-// @version        2016.08.29
+// @version        2016.08.29-1
 // @namespace      http://userscripts.org/scripts/show/59842
 // @description    Can work on almost any webcomic/manga page, preloads 5 or more pages ahead (or behind), navigates via ajax for instant-page-change, lets you use the keyboard, remembers your progress, and it's relatively easy to add new sites
 // @homepageURL    https://github.com/anka-213/webcomic_reader#readme
@@ -4673,8 +4673,8 @@ function fixbadjs(){
 	if ("breakbadtoys" in unsafeWindow) {
 		debugger
 		console.log("Disabling anti-wcr code");
-		window.removeEventListener('load', breakbadtoys, true);
-		breakbadtoys = null;
+		window.removeEventListener('load', unsafeWindow.breakbadtoys, true);
+		unsafeWindow.breakbadtoys = null;
 	}
 }
 
