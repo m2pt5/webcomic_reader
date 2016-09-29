12560626bedae692aaf8e60373da8f231824ba59
Andreas Källberg <anka.213@gmail.com>
2016-09-29 10:47:36 +0200

    Fix http://thedoujin.com/
    Resolves #44

--- webcomic_reader.user.js
+++ webcomic_reader.user.js
@@ -43,7 +43,7 @@ var defaultSettings = {
 // ==UserScript==
 // @name           Webcomic Reader
 // @author         Javier Lopez <ameboide@gmail.com> https://github.com/ameboide , fork by v4Lo https://github.com/v4Lo and by anka-213 http://github.com/anka-213
-// @version        2016.09.1
+// @version        2016.09.29
 // @namespace      http://userscripts.org/scripts/show/59842
 // @description    Can work on almost any webcomic/manga page, preloads 5 or more pages ahead (or behind), navigates via ajax for instant-page-change, lets you use the keyboard, remembers your progress, and it's relatively easy to add new sites
 // @homepageURL    https://github.com/anka-213/webcomic_reader#readme
@@ -458,7 +458,7 @@ var defaultSettings = {
 // @include        http://www.geeklifecomic.com/*
 // @include        http://www.realmofatland.com/*
 // @include        http://realmofatland.com/*
-// @include        http://thedoujin.com/index.php?page=post&s=view&id=*
+// @include        http://thedoujin.com/index.php/pages/*
 // @include        http://eatmanga.com/*
 // @include        http://www.oslevadosdabreca.com/*
 // @include        http://www.thedevilbear.com/*
@@ -2345,9 +2345,11 @@ var paginas = [
 		extra:	[[['.entry']]]
 	},
 	{	url:	'thedoujin.com',
-		img:	'http://img',
-		back:	[/<a href="#" onclick="document.location='([^']+?)'; return false;">Previous/, 1],
-		next:	[/<a href="#" onclick="document.location='([^']+?)'; return false;">Next/, 1],
+		img:	[['#image']],
+		back:	[['.previous > a']],
+		next:	[['.next > a']],
+		first:	[['.first > a']],
+		last:	[['.last > a']],
 		scrollx:'R'
 	},
 	{	url:	'eatmanga.com',
