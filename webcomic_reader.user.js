m2pt5/webcomic_reader

commit 68d56dc9c688eeedb19ab5a9fcd5d9427de4a85e

Implement drop-down menu
A prototype of a solution for #1

webcomic_reader.user.js
@@ -4418,7 +4418,10 @@ var layoutDefault =
 				'<button id="wcr_btn1">Next</button> '+
 				'<a id="wcr_last" href="#">Last &gt;&gt;</a>'+
 			'</div><br/>'+
-			'<div><button id="wcr_btnaddbm" style="background-color:#0f0">Remember this page</button></div>'+
+			'<div>'+
+				'<button id="wcr_btnaddbm" style="background-color:#0f0">Remember this page</button>'+
+				'<select id="wcr_pages" autocomplete="off"><optgroup label="Preloaded pages"/></select> '+
+			'</div>'+
 			'<div id="wcr_listabm"></div>'+
 			'<div id="wcr_div_listabm_todos" style="display:none">'+
 				'<br/><a id="wcr_toggle_bm" href="#toggleBms">See bookmarks for other sites</a>'+
@@ -4783,6 +4786,7 @@ function iniciar(){
 		setEvt(window, 'resize', fitImagen);
 		setEvt('wcr_btn1', 'click', btnnext);
 		setEvt('wcr_btn-1', 'click', btnback);
+		setEvt('wcr_pages', 'change', btnjump);
 		if(clickImgNavigates){
 			setEvt(elemImagen, 'click', imgClick);
 			setEvt(elemImagen, 'mousemove', imgCursor);
@@ -4873,6 +4877,17 @@ function setear(html, pos, dir){
                 }
 			}
 		}
+
+		// Update list of pages
+		try {
+		var pagelist = "";
+		for (var i of Object.keys(titulo).sort((a,b)=>a-b)) {
+			pagelist += '<option value="'+ i +'" title="'+link[i]+'"' +
+				(i==posActual?" selected":"") + '>' +
+				titulo[i]+"</option>";
+		};
+		get("wcr_pages").children[0].innerHTML= pagelist;
+		} catch(e) {}
 
 		extra[pos] = '';
 		if(getExtras){
@@ -4965,6 +4980,9 @@ function cambiaPag(dir, poppedState, slidden){
 		get('wcr_btnaddbm').title = link[posActual];
 		get('wcr_btn1').title = link[posActual + 1] + (imagen[posActual + 1] === null ? ' (image not found)' : '');
 		get('wcr_btn-1').title = link[posActual - 1] + (imagen[posActual - 1] === null ? ' (image not found)' : '');
+
+		// Update list of pages
+		selCss('#wcr_pages option[value="'+posActual+'"]').selected = true;
 
 		var xel = get('wcr_extra');
 		if(keepLayout && extraElement){
@@ -5579,6 +5597,21 @@ function btnback(evt){
 	evt.preventDefault();
 	return false;
 }
+
+//onchange page selector
+function btnjump(evt){
+	debugger;
+	var step = get("wcr_pages").value - posActual;
+	var dir = Math.sign(step);
+	// Jump to one step before the target
+	posActual += step - dir;
+	// And then take a single step
+	if(dir) {cambiaPag(dir);}
+	evt.stopPropagation();
+	evt.preventDefault();
+	return false;
+}
+
 
 //para recordar donde parten los swipes
 var touchpos = {x:0, y:0, t:0};

commit 1b7feb36085b0d67a6f293bad065ed1a325669de

Fix regression for older browsers
Code introduced in 68d56dc caused syntax error browsers not supporting ES6 "for...of" loops.
Fixes #1.

webcomic_reader.user.js
@@ -43,7 +43,7 @@ var defaultSettings = {
 // ==UserScript==
 // @name           Webcomic Reader
 // @author         Javier Lopez <ameboide@gmail.com> https://github.com/ameboide , fork by v4Lo https://github.com/v4Lo and by anka-213 http://github.com/anka-213
-// @version        2016.10.10
+// @version        2016.10.16
 // @namespace      http://userscripts.org/scripts/show/59842
 // @description    Can work on almost any webcomic/manga page, preloads 5 or more pages ahead (or behind), navigates via ajax for instant-page-change, lets you use the keyboard, remembers your progress, and it's relatively easy to add new sites
 // @homepageURL    https://github.com/anka-213/webcomic_reader#readme
@@ -4917,15 +4917,17 @@ function setear(html, pos, dir){
 
 		// Update list of pages
 		try {
-		var pagelist = "";
-		for (var i of Object.keys(titulo).sort(function(a,b){return a-b;})) {
-			if (!isNaN(i)){
-				pagelist += '<option value="'+ i +'" title="'+link[i]+'"' +
-					(i==posActual?" selected":"") + '>' +
-					titulo[i]+"</option>";
+			var pagelist = "";
+			var indices = Object.keys(titulo).sort(function(a,b){return a-b;});
+			for (var j = 0; j < indices.length; j++) {
+				var i = indices[j];
+				if (!isNaN(i)){
+					pagelist += '<option value="'+ i +'" title="'+link[i]+'"' +
+						(i==posActual?" selected":"") + '>' +
+						titulo[i]+"</option>";
+				}
 			}
-		}
-		get("wcr_pages").children[0].innerHTML= pagelist;
+			get("wcr_pages").children[0].innerHTML= pagelist;
 		} catch(e) {}
 
 		extra[pos] = '';